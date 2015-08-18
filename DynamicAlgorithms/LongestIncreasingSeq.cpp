89c0fc98a86709d4616e51f3ef16c2ec4a08c93d
Ravi <ravimathur23@gmail.com>
2015-08-18 15:51:35 +0530

    Create LongestIncreasingSeq.cpp

---
+++ DynamicAlgorithms/LongestIncreasingSeq.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int longestIncreasingSeq(int arr[], int len){
+	
+	int li[len];
+	for(int i=0;i<len;i++)
+		li[i] = 1;
+	
+	for(int i=1;i<len;i++){
+		for(int j=0;j<i;j++){
+			if( (arr[i]>arr[j]) &&  (li[i] < li[j]+1) ){
+				li[i]++;
+			}
+		}
+	}
+	
+	int max = 0;
+	for(int i=0;i<len;i++)
+	{
+		if(max < li[i])
+			max = li[i];
+	}
+	
+	return max;
+}
+
+int main() {
+	
+	int arr[] = {10, 22, 9, 33, 21, 50, 41, 60, 80};
+	int brr[] = {10, 8, 7,11, 2,4,6};
+	int len_arr = sizeof(arr)/sizeof(arr[0]);
+	int len_brr = sizeof(brr)/sizeof(brr[0]);
+	cout<<longestIncreasingSeq(arr, len_arr)<<endl;
+	cout<<longestIncreasingSeq(brr, len_brr)<<endl;
+	return 0;
+}
