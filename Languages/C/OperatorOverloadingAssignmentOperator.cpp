e204d9e5b1aa5b2b6fc233be2f1f16b4b9ecab01
Ravi <ravimathur23@gmail.com>
2015-08-27 08:47:44 +0530

    Update OperatorOverloadingAssignmentOperator.cpp

--- Languages/C/OperatorOverloadingAssignmentOperator.cpp
+++ Languages/C/OperatorOverloadingAssignmentOperator.cpp
@@ -41,9 +41,18 @@ class Test{
 			return *this;
 		}
 		
+		friend Test operator+(const Test& ob1, const Test& ob2);
+		
 		int get_i(){return *i;}
 		void change_i(int num){*i = num;}
 };
+
+Test operator+(const Test& ob1, const Test& ob2){
+	Test ob3;
+	ob3.change_i(*ob1.i + *ob2.i);
+	return ob3;
+}
+
 int main() {
 	Test *ob1 = new Test(5);
 	++(*ob1);
@@ -58,5 +67,7 @@ int main() {
 	ob4 = ob3 = ob2;
 	ob2.change_i(10);
 	cout<<ob3.get_i()<<ob2.get_i()<<ob4.get_i();
+	Test ob5 = ob3+ob4;
+	cout<<endl<<ob5.get_i();
 	return 0;
 }
