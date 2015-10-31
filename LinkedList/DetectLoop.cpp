6edf9fdf1380a358b9082c86ad91cb40b56118da
Ravi <ravimathur23@gmail.com>
2015-10-31 16:55:26 +0530

    Create DetectLoop.cpp

---
+++ LinkedList/DetectLoop.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <map>
+using namespace std;
+class LL{
+    int d;
+    public:
+    LL(int data):d(data){next = NULL;}
+    LL* next;
+    int getD(){return d;}
+};
+void printLL(LL* node){
+    while(1){
+        if(node == NULL)
+            return;
+        cout<<node->getD()<<" ";
+        node = node->next;
+    }
+}
+void detectLoop(LL* node){
+    LL* first = node;
+    LL* second = node->next;
+    int f,s;
+ 
+    if(first->next == NULL){
+        cout<<"No Loop detected"<<endl;
+    }
+    
+    while(1){
+        
+        if(first == second){
+            cout<<"Loop detected";
+            return;
+        }
+        f=1; s=2;
+        while (f) {
+            if(first->next != NULL){
+                first = first->next;
+                f--;
+            }
+            else{
+                cout<<"No Loop detected";
+                return;
+            }
+        }
+        while(s){
+            if(second->next != NULL){
+                second = second->next;
+                s--;
+            }
+            else{
+                cout<<"No Loop detected";
+                return;
+            }
+        }
+        
+    }
+}
+
+int main(){
+    LL *head = new LL(5);
+    head->next = new LL(6);
+    head->next->next = new LL(7);
+    head->next->next->next = new LL(8);
+    head->next->next->next->next  = new LL(9);
+    head->next->next->next->next->next  = new LL(10);
+    head->next->next->next->next->next->next = new LL(11);
+//    head->next->next->next->next->next->next->next = head->next->next->next;
+    
+    detectLoop(head);
+    //printLL(head);cout<<endl;
+
+    return 0;
+}
+
