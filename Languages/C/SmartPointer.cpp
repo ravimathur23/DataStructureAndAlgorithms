83b2e5cfa9cecde7314b9e2d6d09fef069423b02
Ravi <ravimathur23@gmail.com>
2015-12-01 02:24:28 +0530

    Update SmartPointer.cpp

--- Languages/C/SmartPointer.cpp
+++ Languages/C/SmartPointer.cpp
@@ -6,9 +6,7 @@ template<class T>
 class SmartPtr{
     T* ptr;
 public:
-    SmartPtr(T x){
-        ptr = new T();
-        *ptr = x;
+    SmartPtr(T* x):ptr(x){
         cout<<"ctor"<<endl;
     }
     ~SmartPtr(){
@@ -24,8 +22,32 @@ public:
         return *ptr;
     }
 };
+class Rav{
+    int *p;
+public:
+    Rav(int i){p = new int(i);cout<<"ctor ";}
+    Rav(const Rav& ob1){
+        if(this == &ob1)
+            return;
+        if(p == NULL)
+            cout<<"rav"<<endl;
+        p = new int(*ob1.p);cout<<"copy_ctor ";
+    }
+    Rav& operator=(const Rav& ob1){
+        cout<<"assignment_operator ";
+        if(this == &ob1){
+            return *this;
+        }
+        delete p;
+        p = new int(*ob1.p);
+        return *this;
+    }
+    ~Rav(){delete p;cout<<"dest ";}
+    void printVal(){cout<<"*p="<<*p<<endl;}
+};
 int main(){
-    SmartPtr<int> ob1(10);
-    cout<<*ob1<<endl;
+    SmartPtr<Rav> ob1(new Rav(10));
+    ob1->printVal();
+    (*ob1).printVal();
     return 0;
 }
