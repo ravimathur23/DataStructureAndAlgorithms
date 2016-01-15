fab240d5086c4bb9b90aae15f05c393effdda957
Ravi <ravimathur23@gmail.com>
2016-01-15 17:32:50 +0530

    Update PrintRootToLeafPerLine.cpp

--- Tree/PrintRootToLeafPerLine.cpp
+++ Tree/PrintRootToLeafPerLine.cpp
@@ -1,56 +1,64 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 class Tree{
-		int data;
-	public:
-		Tree(int d):data(d){left= NULL; right=NULL;};
-		Tree * left; Tree * right;
-		int get_data(){return data;}
+    int data;
+public:
+    Tree(int d):data(d){left= NULL; right=NULL;};
+    Tree * left; Tree * right;
+    int get_data(){return data;}
 };
-
-void print_path(int num[], int numLen){
-	for(int i=0;i<numLen;i++){
-		cout<<num[i]<<" ";
-	}	
-	cout<<endl;
+void print_inorder(Tree *node){
+    if(node == NULL)
+        return;
+    
+    print_inorder(node->left);
+    cout<<node->get_data()<<" ";
+    print_inorder(node->right);
 }
 
-void print_path_recur(Tree * node, int num[], int numLen){
-	if(node == NULL){
-		return;
-	}
-	
-	num[numLen++] = node->get_data();
-	
-	if(node->left == NULL && node->right == NULL){
-		print_path(num, numLen);
-	}
-	else{
-		print_path_recur(node->left, num, numLen);
-		print_path_recur(node->right, num, numLen);
-	}
+void printRootToLeafUtil(Tree* node, queue<Tree*> q){
+    if(node == NULL)
+        return;
+    
+    q.push(node);
+    printRootToLeafUtil(node->left, q);
+    printRootToLeafUtil(node->right, q);
+    if(node->left == NULL && node->right == NULL)
+    {
+        while(!q.empty())
+        {
+            Tree* tmp = q.front();
+            cout<<tmp->get_data()<<" ";
+            q.pop();
+        }
+        cout<<endl;
+    }
 }
-
-void print_root_to_leaf(Tree * root){
-	int num[100];
-	print_path_recur(root, num, 0);
+void printRootToLeaf(Tree* node){
+    queue<Tree*> q;
+    printRootToLeafUtil(node, q);
 }
 
 int main() {
-	// your code goes here
-	Tree *root = new Tree(20);
-	Tree *left = new Tree(9);
-	Tree *right = new Tree(21);
-	root->left = left;
-	root->right = right;
-	
-	Tree *left_1 = new Tree(8);
-	Tree *right_1 = new Tree(10);
-	root->left->left =left_1;
-	root->left->right = right_1;
-
-	print_root_to_leaf(root);
-	
-	return 0;
+    // your code goes here
+    Tree* root = new Tree(20);
+    root->left = new Tree(10);
+    root->right = new Tree(24);
+    root->left->left = new Tree(8);
+    root->left->right = new Tree(12);
+    root->left->left->left = new Tree(7);
+    root->left->left->right = new Tree(9);
+    root->right->left = new Tree(21);
+    root->right->right = new Tree(27);
+    root->right->right->left = new Tree(25);
+    
+    cout<<"inorder : ";
+    print_inorder(root);
+    cout<<endl;
+    
+    printRootToLeaf(root);
+    
+    return 0;
 }
