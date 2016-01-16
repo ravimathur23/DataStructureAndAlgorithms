6c99a083483afc86de548bcb6cce16dcd8ddf7ab
Ravi <ravimathur23@gmail.com>
2016-01-16 18:24:37 +0530

    Update CheckChildSum.cpp

--- Tree/CheckChildSum.cpp
+++ Tree/CheckChildSum.cpp
@@ -1,48 +1,58 @@
 #include <iostream>
+#include <queue>
+#include <stack>
 using namespace std;
 
 class Tree{
-		int data;
-	public:
-		Tree(int d):data(d){left= NULL; right=NULL;};
-		Tree * left; Tree * right;
-		int get_data(){return data;}
+    int data;
+public:
+    Tree(int d):data(d){left= NULL; right=NULL;};
+    Tree * left; Tree * right;
+    int get_data(){return data;}
 };
 
-bool checkChildSum(Tree *node){
-	
-	if(node){
-		if(!checkChildSum(node->left))
-			return false;
-	
-	if(node->left==NULL || node->right==NULL)
-		return true;
-	else if(node->get_data() == (node->left->get_data()+node->right->get_data()))
-		return true;
-	else
-		return false;
-		
-		
-	return (!checkChildSum(node->right));
-	}
-	
-	return true;
+void print_inorder(Tree *node){
+    if(node == NULL)
+        return;
+    
+    print_inorder(node->left);
+    cout<<node->get_data()<<" ";
+    print_inorder(node->right);
+}
+
+int checkSumChildProperty(Tree* node){
+    if(node == NULL || (node->left == NULL && node->right == NULL))
+        return true;
+    else{
+        int leftData, rightData;
+        if(node->left)
+            leftData = node->left->get_data();
+        else
+            leftData = 0;
+        
+        if(node->right)
+            rightData = node->right->get_data();
+        else
+            rightData = 0;
+        
+        return ((node->get_data() == leftData+ rightData) && checkSumChildProperty(node->left) && checkSumChildProperty(node->right));
+    }
 }
 
 int main() {
-	// your code goes here
-	Tree *root = new Tree(20);
-	Tree *left = new Tree(8);
-	Tree *right = new Tree(12);
-	root->left = left;
-	root->right = right;
-	root->right->right = new Tree(25);	
-	Tree *left_1 = new Tree(3);
-	Tree *right_1 = new Tree(6);
-	root->left->left =left_1;
-	root->left->right = right_1;
-	
-	cout<<checkChildSum(root)<<endl;
-	
-	return 0;
+    // your code goes here
+    Tree* root = new Tree(74);
+    root->left = new Tree(28);
+    root->right = new Tree(46);
+    root->left->left = new Tree(16);
+    root->left->right = new Tree(12);
+    root->left->left->left = new Tree(7);
+    root->left->left->right = new Tree(9);
+    root->right->left = new Tree(21);
+    root->right->right = new Tree(25);
+    root->right->right->left = new Tree(25);
+    
+    cout<<((checkSumChildProperty(root))?"true\n":"false\n");
+    
+    return 0;
 }
